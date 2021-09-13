9f2410589b4df6945690a949e15d0fb95a0e2729
funnykoss <noanybody26@gmail.com>
2021-09-13 19:12:07 +0300

    loader

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,17 @@
+import Loader from 'react-loader-spinner';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import s from './Loader.module.css';
+
+export default function Spinner() {
+  return (
+    <div className={s.loaderContainer}>
+      <Loader
+        className="Loader"
+        type="Plane"
+        color="#5F5FF6"
+        height={100}
+        width={100}
+      />
+    </div>
+  );
+}
