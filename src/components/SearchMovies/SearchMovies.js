07314586b992087aa5da044ab9889120b65f45c5
funnykoss <noanybody26@gmail.com>
2021-09-13 19:11:28 +0300

    searchbar

--- src/components/SearchMovies/SearchMovies.js
+++ src/components/SearchMovies/SearchMovies.js
@@ -1,18 +1,17 @@
 import { useState } from 'react';
 import { toast } from 'react-toastify';
-
 import s from './Searchbar.module.css';
 
 export default function SearchMovies({ onSubmit }) {
   const [query, setQuery] = useState('');
-
-  const handleNameChange = event => {
+  const handleChange = event => {
     setQuery(event.currentTarget.value.toLowerCase());
   };
 
   const handleSubmit = event => {
     event.preventDefault();
-    if (query.trim() === '') {
+
+    if (query.trim === '') {
       toast.error('Enter your query');
       return;
     }
@@ -21,18 +20,18 @@ export default function SearchMovies({ onSubmit }) {
   };
 
   return (
-    <header className={s.Searchbar}>
-      <form onSubmit={handleSubmit} className={s.SearchForm}>
-        <button type="submit" className={s.SearchFormButton}>
-          <span className={s.SearchFormButtonLabel}>Search</span>
+    <header className={s.searchbar}>
+      <form onSubmit={handleSubmit} className={s.searchForm}>
+        <button type="submit" className={s.searchFormButton}>
+          <span className={s.searchFormButtonLabel}>Search</span>
         </button>
 
         <input
-          className={s.SearchFormInput}
           type="text"
           placeholder="Search movies"
-          onChange={handleNameChange}
+          onChange={handleChange}
           value={query}
+          className={s.searchFormInput}
         />
       </form>
     </header>
