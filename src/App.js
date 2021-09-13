b3984519e9d07d3370a65201e452b92c5a27908c
funnykoss <noanybody26@gmail.com>
2021-09-13 19:10:20 +0300

    cast

--- src/App.js
+++ src/App.js
@@ -1,31 +1,46 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { Route, Switch } from 'react-router';
-
+import { lazy, Suspense } from 'react';
+import Loader from 'react-loader-spinner';
 import AppBar from './components/AppBar/AppBar';
-import HomePageView from './views/HomePageView';
-// import MoviesListView from './views/MoviesListView';
+import Container from './components/Container/Container';
+
+// import HomePageView from './views/HomePageView';
+// import MoviesPageSearch from './views/MoviesPageSearch/MoviesPageSearch';
+// import MoviePage from './views/MoviePage/MoviePage';
 import s from './components/AppBar/AppBar.modules.css';
-// import MovieDetailsPageView from './views/MovieDetailsPageView';
-import MoviesPage from './views/MoviesPage';
-import MovieDetailsPageView from './views/MovieDetailsPageView';
+
+const HomePageView = lazy(() =>
+  import('./views/HomePageView' /* webpackChunkName: "home-page" */),
+);
+const MoviesPageSearch = lazy(() =>
+  import(
+    './views/MoviesPageSearch/MoviesPageSearch' /* webpackChunkName: "movieSearch-page" */
+  ),
+);
+const MoviePage = lazy(() =>
+  import('./views/MoviePage/MoviePage' /* webpackChunkName: "movie-page" */),
+);
 
 export default function App() {
   return (
-    <>
+    <Container>
       <AppBar className={s.header} />
-      <Switch>
-        <Route path="/" exact>
-          <HomePageView />
-        </Route>
+      <Suspense fallback={<Loader />}>
+        <Switch>
+          <Route path="/" exact>
+            <HomePageView />
+          </Route>
 
-        <Route path="/movies">
-          <MoviesPage />
-        </Route>
+          <Route path="/movies" exact>
+            <MoviesPageSearch />
+          </Route>
 
-        <Route path="/movies/:movieId">
-          <MovieDetailsPageView />
-        </Route>
-      </Switch>
-    </>
+          <Route path="/movies/:movieId">
+            <MoviePage />
+          </Route>
+        </Switch>
+      </Suspense>
+    </Container>
   );
 }
---
+++ src/components/Cast/Cast.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+import * as fetchAPI from '../../services/movieApi';
+import s from './Cast.module.css';
+
+import photo from '../../img/no_foto.jpg';
+
+export default function Cast({ movieId }) {
+  const [{ cast }, setCast] = useState([]);
+
+  useEffect(() => {
+    fetchAPI.fetchCast(movieId).then(setCast);
+  }, [movieId]);
+
+  return (
+    <ul className={s.cast}>
+      {cast && cast.length > 0 ? (
+        cast.map(({ id, profile_path, name, character }) => (
+          <li key={id} className={s.item}>
+            <img
+              src={
+                profile_path
+                  ? `https://image.tmdb.org/t/p/w300${profile_path}`
+                  : photo
+              }
+              alt={name}
+              className={s.photo}
+            />
+            <p className={s.name}>{name}</p>
+            <p className={s.character}>{character}</p>
+          </li>
+        ))
+      ) : (
+        <b>Nothing found!</b>
+      )}
+    </ul>
+  );
+}
+
+//
